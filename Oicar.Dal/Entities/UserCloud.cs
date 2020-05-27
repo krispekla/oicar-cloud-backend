769f0144d915e1896e1119d70ef7bd661a19cecf
Kris Peklaric <peklarick@gmail.com>
2020-05-27 19:36:28 +0200

    add userCloud

---
+++ Oicar.Dal/Entities/UserCloud.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace Oicar.Dal.Entities
+{
+    public class UserCloud : BaseEntity
+    {
+        [Column(TypeName = "json")]
+        public string UserInput { get; set; }
+
+        public User User { get; set; }
+        public CloudDbSQL CloudDbSQL { get; set; }
+        public CloudFunction CloudFunction { get; set; }
+        public CloudStorage CloudStorage { get; set; }
+        public CloudVM CloudVM { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
