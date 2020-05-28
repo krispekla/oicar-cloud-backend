b0787e1f4a2d1a22488e319033747837d3662601
Kris Peklaric <peklarick@gmail.com>
2020-05-28 23:53:21 +0200

    added

---
+++ Oicar.Service/Interfaces/BaseCloudInterface.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Oicar.Service.Interfaces
+{
+    public interface BaseCloudInterface<TEntity>
+    {
+        TEntity Get(int id);
+        List<TEntity> GetAll();
+        object Add(TEntity entity);
+        object Remove(TEntity entity);
+        object Update(TEntity entity);
+    }
+}
