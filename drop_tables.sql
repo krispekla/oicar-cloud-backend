ec0da9bcb53dcd721534f5d56f828b8c4784448f
Kris Peklaric <peklarick@gmail.com>
2020-05-30 08:10:46 +0200

    images for news, needed to import them to db

---
+++ drop_tables.sql
@@ -0,0 +1,10 @@
+
+DROP TABLE public."NewsPost";
+DROP TABLE public."UserCloud";
+DROP TABLE public."User";
+DROP TABLE public."CloudFunction";
+DROP TABLE public."CloudSQL";
+DROP TABLE public."CloudStorage";
+DROP TABLE public."CloudVM";
+DROP TABLE "__EFMigrationsHistory";
+
